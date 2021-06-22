e2e0dc3bd87c88c35cf8c7e7af8517f18727ceb1
Enkhbat (Eddie) <29717773+enkhbateddie@users.noreply.github.com>
2021-06-22 13:26:33 +1000

    buttons

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(true)
+  const [count, setCount] = useState(false)
+  const [count1, setCount1] = useState(false)
   const handleClick = () => {
-    setCount(count === false ? true : false)
+    setCount(count === true ? false : true)
   }
+  const handleClick1 = () => {
+    setCount1(count1 === true ? false : true)
+  }
+  
   return (
     <div className="App">
       <p>Please choose your video</p>
       <button onClick={handleClick}>upload</button>
+      <br />
+      <br />
+      <button onClick={handleClick1}>convert</button>
     </div>
   );
 }
