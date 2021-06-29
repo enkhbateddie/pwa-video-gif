30c4f63ecfe5c1e31e24e5b192d29e8f5b9f9dac
Enkhbat (Eddie) <29717773+enkhbateddie@users.noreply.github.com>
2021-06-29 15:00:01 +1000

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import './App.css';
 import React, { useState, useEffect } from 'react';
+import './App.css';
+
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
 function App() {
-  const [count, setCount] = useState(false)
-  const [count1, setCount1] = useState(false)
-  const handleClick = () => {
-    setCount(count === true ? false : true)
+  const [message, setMessage] = useState();
+  const [video, setVideo] = useState();
+  const [gif, setGif] = useState();
+
+  const ffmpeg = createFFmpeg({ 
+    log: true,
+    progress: ({ ratio }) => {
+      setMessage(`Complete: ${(ratio * 100.0).toFixed(2)}%`)
+    },
+  });
+
+  const load = async () => {
+    await ffmpeg.load();
   }
-  const handleClick1 = () => {
-    setCount1(count1 === true ? false : true)
+
+  useEffect(() => {
+    load();
+  }, [])
+
+  const convertToGif = async () => {
+    // Write the file to memory 
+    ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
+
+    // Run the FFMpeg command
+    await ffmpeg.run('-i', 'test.mp4', '-t', '2.0', '-ss', '2.0', '-f', 'gif', 'out.gif');
+
+    // Read the result
+    const data = ffmpeg.FS('readFile', 'out.gif');
+
+    // Create a URL
+    const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
+    setGif(url)
   }
-  
+
   return (
     <div className="App">
-      <p>Please choose your video</p>
-      <button onClick={handleClick}>upload</button>
+      { video && <video
+        controls
+        width="250"
+        src={URL.createObjectURL(video)}>
+      </video>}
       <br />
-      <br />
-      <button onClick={handleClick1}>convert</button>
+      <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
+      <h3>Result</h3>
+      <p>{message}</p>
+      <button onClick={convertToGif}>Convert</button>
+      { gif && <img src={gif} width="250" />}
     </div>
-  );
+  )
 }
 
 export default App;
